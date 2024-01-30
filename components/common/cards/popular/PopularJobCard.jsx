9939a8ffa1f133466a7d098c64d33d68c45750f2
shwepyihein <shwepyihein.12@gmail.com>
2024-01-30 15:14:02 +0700

    Job finder application search and detail

---
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -0,0 +1,46 @@
+import { Image, Text, TouchableOpacity, View } from 'react-native';
+
+import { checkImageURL } from '../../../../utils';
+import styles from './popularjobcard.style';
+
+const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
+  if (!item && !item.job_id) return null;
+  return (
+    <TouchableOpacity
+      style={styles.container(selectedJob, item.job_id)}
+      onPress={() => handleCardPress(item)}
+    >
+      <TouchableOpacity style={styles.logoContainer(selectedJob, item.job_id)}>
+        <Image
+          source={{
+            uri: checkImageURL(item?.employer_logo)
+              ? item.employer_logo
+              : 'https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg',
+          }}
+          resizeMode="contain"
+          style={styles.logoImage}
+        />
+      </TouchableOpacity>
+      <Text style={styles.companyName} numberOfLines={1}>
+        {item.employer_name}
+      </Text>
+
+      <View style={styles.infoContainer}>
+        <Text
+          style={styles.jobName(selectedJob, item.job_id)}
+          numberOfLines={1}
+        >
+          {item.job_title}
+        </Text>
+        <View style={styles.infoWrapper}>
+          <Text style={styles.publisher(selectedJob, item.job_id)}>
+            {item?.job_publisher} -
+          </Text>
+          <Text style={styles.location}> {item.job_country}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default PopularJobCard;
---
+++ components/index.js
@@ -0,0 +1,29 @@
+import ScreenHeaderBtn from './common/header/ScreenHeaderBtn';
+
+// home screen
+import Nearbyjobs from './home/nearby/Nearbyjobs';
+import Popularjobs from './home/popular/Popularjobs';
+import Welcome from './home/welcome/Welcome';
+
+// job details screen
+import { default as JobAbout } from './jobdetails/about/About';
+import Company from './jobdetails/company/Company';
+import { default as JobFooter } from './jobdetails/footer/Footer';
+import Specifics from './jobdetails/specifics/Specifics';
+import { default as JobTabs } from './jobdetails/tabs/Tabs';
+
+// common
+import NearbyJobCard from './common/cards/nearby/NearbyJobCard';
+
+export {
+  Company,
+  JobAbout,
+  JobFooter,
+  JobTabs,
+  NearbyJobCard,
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Specifics,
+  Welcome,
+};
